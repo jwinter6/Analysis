0f04c1d04392685ea1c7303412fbe1f8191c6495
Jan Winter <jan.winter@dkfz-heidelberg.de>
2017-03-23 18:09:19 +0100

    finished luciferase

--- VAMBE/app/functions.R
+++ VAMBE/app/functions.R
@@ -9,6 +9,7 @@ plot_luc_analysis_plot_rawdata_FLUC <- function(data = luc_file_data()$plot)
     geom_boxplot() +
     geom_jitter(shape=16, position=position_jitter(0.2)) +
     labs(title = "FLUC", x = "Samples", y = "FLUC") +
+    ggplot2::theme_minimal() +
     theme(axis.text.x = element_text(angle = 45, hjust = 1), plot.title = element_text(size = rel(1.1)))
   return(p)
 }
@@ -20,6 +21,7 @@ plot_luc_analysis_plot_rawdata_RLUC <- function(data = luc_file_data()$plot)
     geom_boxplot() +
     geom_jitter(shape=16, position=position_jitter(0.2)) +
     labs(title = "RLUC", x = "Samples", y = "RLUC") +
+    ggplot2::theme_minimal() +
     theme(axis.text.x = element_text(angle = 45, hjust = 1), plot.title = element_text(size = rel(1.1)))
   return(p)
 }
@@ -30,6 +32,7 @@ plot_luc_analysis_plot_rawdata_RATIO <- function(data = luc_file_data()$plot)
     geom_boxplot() +
     geom_jitter(shape=16, position=position_jitter(0.2)) +
     labs(title = "FLUC normalized by RLUC", x = "Samples", y = "Ratio FLUC/RLUC") +
+    ggplot2::theme_minimal() +
     theme(axis.text.x = element_text(angle = 45, hjust = 1), plot.title = element_text(size = rel(1.1)))
   return(p)
 }
@@ -39,9 +42,179 @@ plot_luc_analysis_plot_rawdata_LOG2RATIO <- function(data = luc_file_data()$plot
   p <- ggplot(data, aes(x=Treatment, y=Log2, fill=Treatment)) + 
     geom_boxplot() +
     geom_jitter(shape=16, position=position_jitter(0.2)) +
+    ggplot2::theme_minimal() +
     labs(title = "Log2 FLUC normalized by RLUC", x = "Samples", y = "Log2 Ratio FLUC/RLUC") +
     theme(axis.text.x = element_text(angle = 45, hjust = 1), plot.title = element_text(size = rel(1.1)))
   return(p)
 }
 
 
+plot_luc_analysis_plot_individual_FLUC <- function(data = luc_file_data()$plot, groups = NULL, calibrator = NULL)
+{
+  
+    if(!is.null(groups))
+    {
+      
+      if(!is.null(calibrator))
+      {
+        
+        # calibrate to sample
+        # divide FLUC by the calibrator
+        data$FLUC <- data$FLUC/median(dplyr::filter(data, Treatment %in% calibrator)$FLUC, na.rm = TRUE)
+        data_cal <- dplyr::filter(data, Treatment %in% calibrator)
+        data <- dplyr::filter(data, Treatment %in% groups)
+        data <- dplyr::bind_rows(data, data_cal)
+        title  = paste("FLUC - Data calibrated to ", calibrator, sep="")
+        xlab = "Samples"
+        ylab = "Fold Change FLUC"
+
+      } else {
+        data <- dplyr::filter(data, Treatment %in% groups)
+        title  = "FLUC"
+        xlab = "Samples"
+        ylab = "FLUC"
+      }
+      
+      p <- ggplot(data, aes(x=Treatment, y=FLUC, fill=Treatment)) + 
+        geom_boxplot() +
+        geom_jitter(shape=16, position=position_jitter(0.2)) +
+        labs(title = title, x = xlab, y = ylab) +
+        ggplot2::theme_minimal() +
+        theme(axis.text.x = element_text(angle = 45, hjust = 1), plot.title = element_text(size = rel(1.1)))
+      return(p)
+      
+    } else {
+      return("No Groups Selected")
+    }
+ 
+ 
+}
+
+plot_luc_analysis_plot_individual_RLUC <- function(data = luc_file_data()$plot, groups = NULL, calibrator = NULL)
+{
+
+    if(!is.null(groups))
+    {
+      
+      
+      if(!is.null(calibrator))
+      {
+        
+        # calibrate to sample
+        # divide RLUC by the calibrator
+        data$RLUC <- data$RLUC/median(dplyr::filter(data, Treatment %in% calibrator)$RLUC, na.rm = TRUE)
+        data_cal <- dplyr::filter(data, Treatment %in% calibrator)
+        data <- dplyr::filter(data, Treatment %in% groups)
+        data <- dplyr::bind_rows(data, data_cal)
+        
+        title  = paste("RLUC - Data calibrated to ", calibrator, sep="")
+        xlab = "Samples"
+        ylab = "Fold Change RLUC"
+        
+      } else {
+        data <- dplyr::filter(data, Treatment %in% groups)
+        title  = "FLUC"
+        xlab = "Samples"
+        ylab = "RLUC"
+      }
+      
+      p <- ggplot(data, aes(x=Treatment, y=RLUC, fill=Treatment)) + 
+        geom_boxplot() +
+        geom_jitter(shape=16, position=position_jitter(0.2)) +
+        labs(title = title, x = xlab, y = ylab) +
+        ggplot2::theme_minimal() +
+        theme(axis.text.x = element_text(angle = 45, hjust = 1), plot.title = element_text(size = rel(1.1)))
+      return(p)
+     
+    } else {
+      return("No Groups Selected")
+    }
+  
+ 
+  
+}
+
+plot_luc_analysis_plot_individual_RATIO <- function(data = luc_file_data()$plot, groups = NULL, calibrator = NULL)
+{
+ 
+    if(!is.null(groups))
+    {
+      
+      if(!is.null(calibrator))
+      {
+        
+        # calibrate to sample
+        # divide RATIO by the calibrator
+        data$Divided <- data$Divided/median(dplyr::filter(data, Treatment %in% calibrator)$Divided, na.rm = TRUE)
+        data_cal <- dplyr::filter(data, Treatment %in% calibrator)
+        data <- dplyr::filter(data, Treatment %in% groups)
+        data <- dplyr::bind_rows(data, data_cal)
+        
+        title  = paste("Ratio - Data calibrated to ", calibrator, sep="")
+        xlab = "Samples"
+        ylab = "Fold Change Ratio FLUC/RLUC"
+        
+      } else {
+        data <- dplyr::filter(data, Treatment %in% groups)
+        title  = "Ratio FLUC/RLUC"
+        xlab = "Samples"
+        ylab = "Ratio FLUC/RLUC"
+      }
+      
+      p <- ggplot(data, aes(x=Treatment, y=Divided, fill=Treatment)) + 
+        geom_boxplot() +
+        geom_jitter(shape=16, position=position_jitter(0.2)) +
+        labs(title = title, x = xlab, y = ylab) +
+        ggplot2::theme_minimal() +
+        theme(axis.text.x = element_text(angle = 45, hjust = 1), plot.title = element_text(size = rel(1.1)))
+      return(p)
+     
+    } else {
+      return("No Groups Selected")
+    }
+
+}
+
+plot_luc_analysis_plot_individual_LOG2RATIO <- function(data = luc_file_data()$plot, groups = NULL, calibrator = NULL)
+{
+    if(!is.null(groups))
+    {
+      
+      
+      
+      if(!is.null(calibrator))
+      {
+        
+        # calibrate to sample
+        # divide FLUC by the calibrator
+        
+        data$Log2 <- data$Log2/median(dplyr::filter(data, Treatment %in% calibrator)$Log2, na.rm = TRUE)
+        data_cal <- dplyr::filter(data, Treatment %in% calibrator)
+        data <- dplyr::filter(data, Treatment %in% groups)
+        data <- dplyr::bind_rows(data, data_cal)
+        
+        title  = paste("Log2 FLUC normalized by RLUC - Data calibrated to ", calibrator, sep="")
+        xlab = "Samples"
+        ylab = "Fold Change Log2 Ratio FLUC/RLUC"
+        
+      } else {
+        data <- dplyr::filter(data, Treatment %in% groups)
+        title  = "FLUC"
+        xlab = "Samples"
+        ylab = "Log2 Ratio FLUC/RLUC"
+      }
+      
+      p <- ggplot(data, aes(x=Treatment, y=Log2, fill=Treatment)) + 
+        geom_boxplot() +
+        geom_jitter(shape=16, position=position_jitter(0.2)) +
+        labs(title = title, x = xlab, y = ylab) +
+        ggplot2::theme_minimal() +
+        theme(axis.text.x = element_text(angle = 45, hjust = 1), plot.title = element_text(size = rel(1.1)))
+      return(p)
+      
+      
+    } else {
+      return("No Groups Selected")
+    }
+
+}
--- VAMBE/app/luc_analysis.R
+++ VAMBE/app/luc_analysis.R
@@ -15,15 +15,84 @@ tabItem(tabName = "luc_analysis", align = "center",
                                  plotOutput("luc_analysis_plot_rawdata_FLUC")
                                ),
                                tabPanel("RLUC",
+                                        downloadButton("DL_luc_analysis_plot_rawdata_RLUC", label = "Download Plot"),
                                         plotOutput("luc_analysis_plot_rawdata_RLUC")
                                         ),
                                tabPanel("Ratio",
+                                        downloadButton("DL_luc_analysis_plot_rawdata_RATIO", label = "Download Plot"),
                                         plotOutput("luc_analysis_plot_rawdata_RATIO")
                                         ),
                                tabPanel("Log2 Ratio",
+                                        downloadButton("DL_luc_analysis_plot_rawdata_LOG2RATIO", label = "Download Plot"),
                                         plotOutput("luc_analysis_plot_rawdata_LOG2RATIO")
                                         )
+                               ),
+                        
+                        column(width=12,
+                               column(width=6, offset=3,
+                                      box(width=12, solidHeader = TRUE, collapsible = FALSE, status="primary", title = "Please select the treatments to plot",
+                                          uiOutput("luc_analysis_rawdata_select")
+                                      )
+                                      ),
+
+                        tabBox(width=12,
+                               title = "Raw Data User-Defined",
+                               
+                               
+                               
+                               tabPanel( "FLUC",
+                                         downloadButton("DL_luc_analysis_plot_individual_FLUC", label = "Download Plot"),
+                                         plotOutput("luc_analysis_plot_individual_FLUC")
+                               ),
+                               tabPanel("RLUC",
+                                        downloadButton("DL_luc_analysis_plot_individual_RLUC", label = "Download Plot"),
+                                        plotOutput("luc_analysis_plot_individual_RLUC")
+                               ),
+                               tabPanel("Ratio",
+                                        downloadButton("DL_luc_analysis_plot_individual_RATIO", label = "Download Plot"),
+                                        plotOutput("luc_analysis_plot_individual_RATIO")
+                               ),
+                               tabPanel("Log2 Ratio",
+                                        downloadButton("DL_luc_analysis_plot_individual_LOG2RATIO", label = "Download Plot"),
+                                        plotOutput("luc_analysis_plot_individual_LOG2RATIO")
                                )
+                        )
+                        ),
+                        
+                        
+                        column(width=12,
+                               column(width=6, offset=3,
+                                      box(width=12, solidHeader = TRUE, collapsible = FALSE, status="primary", title = "Please select the calibrator",
+                                          shiny::helpText("Select the treatment to which you want to calibrate all of the selected samples above to."),
+                                          uiOutput("luc_analysis_calibrator_select")
+                                      )
+                               ),
+                               tabBox(width=12,
+                                      title = "Calibrated Sets",
+                                      
+                                      
+                                      
+                                      tabPanel( "FLUC",
+                                                downloadButton("DL_luc_analysis_plot_calibrated_FLUC", label = "Download Plot"),
+                                                plotOutput("luc_analysis_plot_calibrated_FLUC")
+                                      ),
+                                      tabPanel("RLUC",
+                                               downloadButton("DL_luc_analysis_plot_calibrated_RLUC", label = "Download Plot"),
+                                               plotOutput("luc_analysis_plot_calibrated_RLUC")
+                                      ),
+                                      tabPanel("Ratio",
+                                               downloadButton("DL_luc_analysis_plot_calibrated_RATIO", label = "Download Plot"),
+                                               plotOutput("luc_analysis_plot_calibrated_RATIO")
+                                      ),
+                                      tabPanel("Log2 Ratio",
+                                               downloadButton("DL_luc_analysis_plot_calibrated_LOG2RATIO", label = "Download Plot"),
+                                               plotOutput("luc_analysis_plot_calibrated_LOG2RATIO")
+                                               
+                                      )
+                               )
+                               
+                        )
+                        
                         
         )
         
